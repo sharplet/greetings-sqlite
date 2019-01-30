d8baaab8410a9735eada4ce2f92765390763c3d4
Adam Sharp <adam@sharplet.me>
2019-01-29 23:05:43 -0500

    Only ever uppercase the first letter in the string
    Using this method on a string such as "123 is a number" would give the
    result "123 Is a number", which is surprising. This much simpler
    implementation will only ever uppercase the first character.

--- Sources/SQLite/StringExtensions.swift
+++ Sources/SQLite/StringExtensions.swift
@@ -1,10 +1,6 @@
 extension String {
   func firstLetterUppercased() -> String {
-    guard let letter = firstIndex(where: { $0.isLetter }), !self[letter].isUppercase else { return self }
-
-    var result = String(self[..<letter])
-    result += self[letter].uppercased()
-    result += self[index(after: letter)...]
-    return result
+    guard let first = first, first.isLowercase else { return self }
+    return String(first).uppercased() + dropFirst()
   }
 }
